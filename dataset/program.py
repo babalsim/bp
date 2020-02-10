ca4dd447b215aa4231b2007a328f4119b9e8a72e
babalsim <simon.babal@outlook.com>
2020-02-10 15:18:11 +0100

    updated first chapter in thesis and added sources

---
+++ dataset/program.py
@@ -0,0 +1,68 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+kernel = np.array([[-1,-1,-1], 
+                   [-1, 9,-1],
+                   [-1,-1,-1]])
+
+img = cv.imread('klaviatura2.png')
+img = cv.filter2D(img, -1, kernel)
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+
+
+ret, thresh = cv.threshold(gray,70,255,cv.THRESH_BINARY)
+
+
+# noise removal
+kernel = np.ones((3,3),np.uint8)
+opening = cv.morphologyEx(thresh,cv.MORPH_OPEN,kernel, iterations = 2)
+# sure background area
+sure_bg = cv.dilate(opening,kernel,iterations=3)
+
+# Finding sure foreground area
+dist_transform = cv.distanceTransform(opening,cv.DIST_L2,5)
+ret, sure_fg = cv.threshold(dist_transform,0.7*dist_transform.max(),255,0)
+# Finding unknown region
+sure_fg = np.uint8(sure_fg)
+unknown = cv.subtract(sure_bg,sure_fg)
+
+# Marker labelling
+ret, markers = cv.connectedComponents(sure_fg)
+# Add one to all labels so that sure background is not 0, but 1
+markers = markers+1
+# Now, mark the region of unknown with zero
+markers[unknown==255] = 0
+
+markers = cv.watershed(img,markers)
+img[markers == -1] = [255,0,0]
+
+#mark = np.zeros(markers.shape, dtype=np.uint8)
+mark = markers.astype('uint8')
+mark = cv.bitwise_not(mark)
+# uncomment this if you want to see how the mark
+# image looks like at that point
+#cv.imshow('Markers_v2', mark)
+# Generate random colors
+colors = []
+for contour in contours:
+    colors.append((rng.randint(0,256), rng.randint(0,256), rng.randint(0,256)))
+# Create the result image
+dst = np.zeros((markers.shape[0], markers.shape[1], 3), dtype=np.uint8)
+# Fill labeled objects with random colors
+for i in range(markers.shape[0]):
+    for j in range(markers.shape[1]):
+        index = markers[i,j]
+        if index > 0 and index <= len(contours):
+            dst[i,j,:] = colors[index-1]
+# Visualize the final image
+cv.imshow('Final Result', dst)
+
+cv.imshow('nieco', markers)
+
+
+
+
+
+cv.waitKey(0)
