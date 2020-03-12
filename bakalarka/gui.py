abef96cb5c262f7d9b90989a6e6210130fb60e49
babalsim <simon.babal@outlook.com>
2020-03-12 14:31:24 +0100

    refactoring

--- bakalarka/gui.py
+++ bakalarka/gui.py
@@ -1,3 +1,104 @@
+from tkinter import Frame, Canvas, Button, Scale, DoubleVar, Checkbutton, BooleanVar, HORIZONTAL, Tk, messagebox
+import cv2 as cv
+
+
 class Gui:
-    def __init__(self, main):
+    fps = None
+    frame_count = None
+    duration = None
+
+    def __init__(self, main, size_x, size_y):
         self.main = main
+        self.SIZE_X = size_x
+        self.SIZE_Y = size_y
+        self._initGUI()
+
+    def _initGUI(self):
+        self.master = Tk()
+        self.master.title('Bakalarka')
+        self._initFrameCanvasGUI()
+        self._initLoadResetGUI()
+        self._initExportGUI()
+        self._initTimeControlGUI()
+        self._initBackgroundCropGUI()
+        self._initPlayMarkGUI()
+        self._initCheckButtons()
+
+    def _initFrameCanvasGUI(self):
+        self.frame = Frame(self.master, width=self.SIZE_X + 250, height=self.SIZE_X * 2 + 100)
+        self.frame.pack()
+        self.canvas = Canvas(self.frame, width=self.SIZE_X, height=self.SIZE_Y)
+        self.canvas.place(x=0, y=0)
+
+    def _initLoadResetGUI(self):
+        Button(self.frame, text='Load', padx=10, pady=1, command=self.main.load).place(
+            x=self.SIZE_X + 5, y=5)
+        Button(self.frame, text='Reset', padx=10, pady=1, command=self.main.reset).place(
+            x=self.SIZE_X + 115, y=5)
+
+    def _initExportGUI(self):
+        Button(self.frame, text='Export to MIDI', padx=10, pady=1, command=self.main.exportMIDI).place(
+            x=self.SIZE_X + 5, y=45)
+        Button(self.frame, text='Export to MusicXML', padx=10, pady=1, command=self.main.exportMusicXML).place(
+            x=self.SIZE_X + 115, y=45)
+
+    def _initTimeControlGUI(self):
+        self.position = DoubleVar(0)
+        self.scale = Scale(self.frame, label='----', from_=0, orient=HORIZONTAL,
+                           length=self.SIZE_X, tickinterval=15, command=self.updatePosition, variable=self.position)
+        self.scale.place(x=20, y=self.SIZE_X + 20)
+
+    def _initBackgroundCropGUI(self):
+        Button(self.frame, text='Set Background', padx=10, pady=1, command=self.main.setBackground).place(
+            x=self.SIZE_X + 5, y=95)
+        Button(self.frame, text='Crop Keyboard', padx=10, pady=1, command=self.main.cropKeyboardArea).place(
+            x=self.SIZE_X + 115, y=95)
+
+    def _initPlayMarkGUI(self):
+        self.playOrStopButton = Button(self.frame, text='Play', padx=10, pady=1, command=self.playOrStop)
+        self.playOrStopButton.place(x=self.SIZE_X + 5, y=135)
+        Button(self.frame, text='Mark C', padx=10, pady=1, command=self.main.markKeyboardMiddle).place(
+            x=self.SIZE_X + 115, y=135)
+
+    def _initCheckButtons(self):
+        self.handFilter = BooleanVar()
+        self.handFilter.set(False)
+        Checkbutton(self.frame, text="HandFilter", variable=self.handFilter).place(x=self.SIZE_X + 5, y=165)
+        self.transcribing = BooleanVar()
+        self.transcribing.set(False)
+        Checkbutton(self.frame, text="Transcribing", variable=self.transcribing).place(x=self.SIZE_X + 5, y=185)
+
+    def playOrStop(self):
+        if self.main.playing:
+            self.stop()
+        elif self.main.capture is not None and self.main.capture.isOpened():
+            self.play()
+        else:
+            messagebox.showinfo('Chyba', 'Nie je načítané video.')
+
+    def play(self):
+        self.main.playing = True
+        self.playOrStopButton['text'] = 'Stop'
+        while self.main.playing:
+            try:
+                self.main.step()
+            except FileNotFoundError:######################################neskor prepisat na TypeError
+                self.stop()
+
+    def stop(self):
+        self.main.playing = False
+        self.playOrStopButton['text'] = 'Play'
+
+    def setTimeControlGUI(self):
+        self.fps = self.main.capture.get(cv.CAP_PROP_FPS)
+        self.frame_count = int(self.main.capture.get(cv.CAP_PROP_FRAME_COUNT))
+        self.duration = self.frame_count / self.fps
+        self.scale['to'] = self.duration
+
+    def updatePosition(self, position):
+        frameNumber = int(position) * self.fps
+        self.main.capture.set(cv.CAP_PROP_POS_FRAMES, frameNumber)
+        self.main.showFrame()
+
+    def showPosition(self):
+        self.position.set(self.main.capture.get(cv.CAP_PROP_POS_MSEC) / 1000)
--- bakalarka/main.py
+++ bakalarka/main.py
@@ -1,15 +1,14 @@
-#pip install opencv-python MIDIUtil tk
+# pip install opencv-python MIDIUtil tk
 
 import numpy as np
 import cv2 as cv
 import time
-from tkinter import filedialog, messagebox, Frame, Canvas, Button, Scale, DoubleVar, Checkbutton, BooleanVar
+from tkinter import filedialog, messagebox, NW
 from os import path
-from tkinter import Tk, NW, HORIZONTAL
-from PIL import Image, ImageTk
 from capture import Capture
 from cropper import Cropper
 from marker import Marker
+from gui import Gui
 
 
 class Program:
@@ -27,81 +26,20 @@ class Program:
     rel = []
 
     def __init__(self, debug=False):
-        self._initGUI()
-        if debug:
-            self.load('../dataset/samplek.mp4')
-            self.capture.y_start, self.capture.y_end = 273, 432
-            self.capture.x_start, self.capture.x_end = 200, 772
-            self.capture.x_middle, self.capture.y_middle = 450, 390
-            self.drawFrame()
-            self.setBackground()
-        self.master.update()
-        self.master.mainloop()
+        self.gui = Gui(self, self.SIZE_X, self.SIZE_Y)
+        # if debug:
+        #     self.load('../dataset/samplek.mp4')
+        #     self.capture.y_start, self.capture.y_end = 273, 432
+        #     self.capture.x_start, self.capture.x_end = 200, 772
+        #     self.capture.x_middle, self.capture.y_middle = 450, 390
+        #     self.drawFrame()
+        #     self.setBackground()
+        self.gui.master.update()
+        self.gui.master.mainloop()
 
     def __del__(self):
         if self.capture.isOpened():
             self.capture.release()
-
-    def _initGUI(self):
-        self.master = Tk()
-        self.master.title('Bakalarka')
-        self._initFrameCanvasGUI()
-        self._initLoadResetGUI()
-        self._initExportGUI()
-        self._initTimeControlGUI()
-        self._initBackgroundCropGUI()
-        self._initPlayMarkGUI()
-        self._initCheckButtons()
-
-    def _initFrameCanvasGUI(self):
-        self.frame = Frame(self.master, width=self.SIZE_X + 250, height=self.SIZE_X * 2 + 100)
-        self.frame.pack()
-        self.canvas = Canvas(self.frame, width=self.SIZE_X, height=self.SIZE_Y)
-        self.canvas.place(x=0, y=0)
-
-    def _initLoadResetGUI(self):
-        Button(self.frame, text='Load', padx=10, pady=1, command=self.load).place(
-            x=self.SIZE_X + 5, y=5)
-        Button(self.frame, text='Reset', padx=10, pady=1, command=self.reset).place(
-            x=self.SIZE_X + 115, y=5)
-
-    def _initExportGUI(self):
-        Button(self.frame, text='Export to MIDI', padx=10, pady=1, command=self.exportMIDI).place(
-            x=self.SIZE_X + 5, y=45)
-        Button(self.frame, text='Export to MusicXML', padx=10, pady=1, command=self.exportMusicXML).place(
-            x=self.SIZE_X + 115, y=45)
-
-    def _initTimeControlGUI(self):
-        self.position = DoubleVar(0)
-        self.scale = Scale(self.frame, label='----', from_=0, orient=HORIZONTAL,
-                           length=self.SIZE_X, tickinterval=15, command=self.updatePosition, variable=self.position)
-        self.scale.place(x=20, y=self.SIZE_X + 20)
-
-    def _initBackgroundCropGUI(self):
-        Button(self.frame, text='Set Background', padx=10, pady=1, command=self.setBackground).place(
-            x=self.SIZE_X + 5, y=95)
-        Button(self.frame, text='Crop Keyboard', padx=10, pady=1, command=self.cropKeyboardArea).place(
-            x=self.SIZE_X + 115, y=95)
-
-    def _initPlayMarkGUI(self):
-        self.playOrStopButton = Button(self.frame, text='Play', padx=10, pady=1, command=self.playOrStop)
-        self.playOrStopButton.place(x=self.SIZE_X + 5, y=135)
-        Button(self.frame, text='Mark C', padx=10, pady=1, command=self.markKeyboardMiddle).place(
-            x=self.SIZE_X + 115, y=135)
-
-    def _initCheckButtons(self):
-        self.handFilter = BooleanVar()
-        self.handFilter.set(False)
-        Checkbutton(self.frame, text="HandFilter", variable=self.handFilter).place(x=self.SIZE_X + 5, y=165)
-        self.transcribing = BooleanVar()
-        self.transcribing.set(False)
-        Checkbutton(self.frame, text="Transcribing", variable=self.transcribing).place(x=self.SIZE_X + 5, y=185)
-
-    def _setTimeControlGUI(self):
-        self.fps = self.capture.get(cv.CAP_PROP_FPS)
-        self.frame_count = int(self.capture.get(cv.CAP_PROP_FRAME_COUNT))
-        self.duration = self.frame_count / self.fps
-        self.scale['to'] = self.duration
 
     def load(self, filename=''):
         try:
@@ -111,33 +49,17 @@ class Program:
                 raise FileNotFoundError
             self.capture.setVideoFile(filename)
             self.capture.setCanvasSize(self.SIZE_X, self.SIZE_Y)
-            self._setTimeControlGUI()
+            self.gui.setTimeControlGUI()
             self.showFrame()
         except FileNotFoundError:
             messagebox.showinfo("Chyba", "Súbor sa nepodarilo prečítať")
 
-    def showProcessed(self, frame):
-        canvas = Canvas(self.frame, width=self.SIZE_X, height=self.SIZE_X)
-        canvas.place(x=0, y=self.SIZE_Y)
-        photo = ImageTk.PhotoImage(image=Image.fromarray(cv.cvtColor(frame, cv.COLOR_BGR2RGB)).resize(
-            (self.SIZE_X, self.SIZE_X), Image.ANTIALIAS))
-        canvas.create_image(0, 0, image=photo, anchor=NW)
-
-    def play(self):
-        self.playing = True
-        self.playOrStopButton['text'] = 'Stop'
-        while self.playing:
-            try:
-                self._step()
-            except FileNotFoundError:
-                self.stop()
-
-    def _step(self):
+    def step(self):
         self.capture.grab()
-        if self.transcribing.get() and Frame:
+        if self.gui.transcribing.get():
             self.transcribe()
         self.showFrame()
-        self.showPosition()
+        self.gui.showPosition()
 
     def transcribe(self):
         frame = cv.cvtColor(self.capture.getCurrentFrameCropped(), cv.COLOR_BGR2GRAY)
@@ -147,7 +69,7 @@ class Program:
             for x, y in points:
                 if self.capture.grayBackground[y][x] != frame[y][x]:
                     dif += 1
-            if dif/len(points) > 0.97:
+            if dif / len(points) > 0.97:
                 pressed.add(key)
         self.detectBlackPressed(pressed)
 
@@ -167,15 +89,11 @@ class Program:
         self.rel.append(key)
         print(f'{key} pressed for {duration}')
 
-    def stop(self):
-        self.playing = False
-        self.playOrStopButton['text'] = 'Play'
-
     def showFrame(self):
         self.drawFrame()
 
     def drawFrame(self):
-        if self.transcribing.get() and False:
+        if self.gui.transcribing.get() and False:
             frame1 = cv.cvtColor(self.capture.background, cv.COLOR_BGR2GRAY)
             frame2 = cv.cvtColor(self.capture.getCurrentFrameCropped(), cv.COLOR_BGR2GRAY)
             self.currentFrame = self.capture.getSubtractedFramePhotoImage(frame1, frame2)
@@ -184,14 +102,14 @@ class Program:
         self.drawImage(self.currentFrame)
 
     def drawImage(self, image):
-        self.canvas.create_image(0, 0, image=image, anchor=NW)
-        self.frame.update()
+        self.gui.canvas.create_image(0, 0, image=image, anchor=NW)
+        self.gui.frame.update()
 
     def segmentation(self, t=180):
         gray = cv.cvtColor(self.capture.background, cv.COLOR_BGR2GRAY)
         gray = cv.GaussianBlur(gray, (3, 3), 0)
         ret, thresh = cv.threshold(gray, t, 255, cv.THRESH_BINARY_INV)
-        #ret, thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3)
+        # ret, thresh = cv.adaptiveThreshold(gray,255,cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3)
         kernel = np.ones((9, 9), np.uint8)
         opening = cv.morphologyEx(thresh, cv.MORPH_OPEN, kernel, iterations=1)
         dist_transform = cv.distanceTransform(opening, cv.DIST_L2, 5)
@@ -288,26 +206,6 @@ class Program:
 
     def mapWhiteKeys(self):
         raise NotImplementedError()
-
-    def playOrStop(self):
-        if self.playing:
-            self.stop()
-        elif self.capture is not None and self.capture.isOpened():
-            self.play()
-        else:
-            messagebox.showinfo('Chyba', 'Nie je načítané video.')
-
-    def updatePosition(self, position):
-
-        frameNumber = int(position) * self.fps
-        self.capture.set(cv.CAP_PROP_POS_FRAMES, frameNumber)
-        self.showFrame()
-
-    def showPosition(self):
-        tmp = self.position.get()
-        self.position.set(self.capture.get(cv.CAP_PROP_POS_MSEC) / 1000)
-        if round(self.position.get()) > round(tmp):
-            print(self.position.get())
 
     def cropKeyboardArea(self):
         Cropper(self.capture)
